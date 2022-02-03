1c9b55c5497c330ccbeabfd52e899f5de82b6153
Hasitha Adikari <adhikari@globalwavenet.com>
2022-02-03 14:44:37 +0530

    first commit

---
+++ src/main/java/com/example/departmentservice/service/DepartmentServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.departmentservice.service;
+
+import com.example.departmentservice.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepartmentServiceImpl implements DepartmentService{
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+}
